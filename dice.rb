8e5782186e965fd4fe03659b05e1e6d24d70df4e
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-05-17 15:55:51 +0000

    get route "/dice/2/6"

--- dice.rb
+++ dice.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sinatra/reloader"
 
  get("/") {
   "Hello world"
@@ -7,3 +8,20 @@ require "sinatra"
 get("zebra") do
   "Add a route"
 end
+
+get("/one/two") {
+  "Two layers"
+}
+
+dice_count = 2
+dice_side = 6
+
+get("/dice/2/6") {
+  num1 = rand(1..6)
+  num2 = rand(1..6)
+  sum = num1 + num2
+  result = "You rolled a #{num1} and a #{num2} for a total of #{sum}."
+
+  return "<h1>2d6</h1>
+  <p>#{result}</p>"
+}
